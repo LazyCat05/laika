84b629f862a65db63832f0571a778b897ed46d2e
Tim Hollis <tim.hollis@gmail.com>
2018-05-09 20:23:53 -0400

    coordinate function works for outer planets

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -18,12 +18,21 @@ class Planet < ApplicationRecord
     longitude_of_perihelion - ascending_node_longitude
   end
 
-  def mean_anomaly(mean_longitude, longitude_of_perihelion)
-    value = (mean_longitude - longitude_of_perihelion)
+  def mean_anomaly(mean_longitude, argument_of_perihelion)
+    value = (mean_longitude - argument_of_perihelion)
     if value < 0
       value += 360
     end
     return value
+  end
+
+  def mean_anomaly_outer_planet(mean_longitude, argument_of_perihelion, b_outer_correction, c_outer_correction, f_outer_correction, s_outer_correction, date)
+    julian_date = (date.ajd.to_f - 2451545.0)/36525
+    if c_outer_correction == nil || f_outer_correction == nil || s_outer_correction == nil
+      value = value = mean_longitude - argument_of_perihelion + b_outer_correction * julian_date ** 2 
+    else
+      value = mean_longitude - argument_of_perihelion + b_outer_correction * julian_date ** 2 + c_outer_correction * Math.cos(f_outer_correction * julian_date) + s_outer_correction * Math.sin(f_outer_correction * julian_date)
+    end
   end
 
   def perihelion_distance(semimajor_axis, eccentricity)
@@ -103,55 +112,35 @@ class Planet < ApplicationRecord
   end
 
   def coordinates(date)
-    puts self.name
     planet_mean_longitude = calculate_parameter(self.mean_longitude, self.mean_longitude_correction, date) % 360
-    puts "L = #{planet_mean_longitude}"
     planet_semimajor_axis = calculate_parameter(self.semimajor_axis, self.semimajor_axis_correction, date)
-    puts "a = #{planet_semimajor_axis}"
     planet_eccentricity = calculate_parameter(self.eccentricity, self.eccentricity_correction, date)
-    puts "e = #{planet_eccentricity}"
     planet_inclination = calculate_parameter(self.inclination, self.inclination_correction, date)
-    puts "i = #{planet_inclination}"
     planet_longitude_of_perihelion = calculate_parameter(self.longitude_of_perihelion, self.longitude_of_perihelion_correction, date)
-    puts "p = #{planet_longitude_of_perihelion}"
     planet_longitude_of_the_ascending_node = calculate_parameter(self.ascending_node_longitude, self.ascending_node_longitude_correction, date)
-    puts "W = #{planet_longitude_of_the_ascending_node}"
     planet_argument_of_perihelion = calc_argument_of_perihelion(planet_longitude_of_perihelion, planet_longitude_of_the_ascending_node)
-    puts "w = #{planet_argument_of_perihelion}"
-    planet_mean_anomaly = mean_anomaly(planet_mean_longitude, planet_longitude_of_perihelion)
-    puts "M = #{planet_mean_anomaly}"
+    planet_mean_anomaly = nil
+    if !outer_planet_correction
+      planet_mean_anomaly = mean_anomaly(planet_mean_longitude, planet_longitude_of_perihelion)
+    else
+      planet_mean_anomaly = mean_anomaly_outer_planet(planet_mean_longitude, planet_longitude_of_perihelion, self.b_outer_correction, self.c_outer_correction, self.f_outer_correction, self.s_outer_correction, date)
+    end
     planet_perihelion = perihelion_distance(planet_semimajor_axis, planet_eccentricity)
-    puts "R(p) = #{planet_perihelion}"
     planet_aphelion = aphelion_distance(planet_semimajor_axis, planet_eccentricity)
-    puts "R(a) = #{planet_aphelion}"
     planet_eccentric_anomaly = eccentric_anomaly(planet_mean_anomaly, planet_eccentricity, planet_longitude_of_perihelion)
-    puts "E = #{planet_eccentric_anomaly}"
     planet_true_anomaly = true_anomaly(planet_semimajor_axis, planet_eccentric_anomaly, planet_eccentricity)
-    puts "n = #{planet_true_anomaly}"
     planet_radius_vector = radius_vector(planet_semimajor_axis, planet_eccentricity, planet_eccentricity)
-    puts "r_v = #{planet_radius_vector}"
     planet_distance_from_primary = distance(planet_semimajor_axis, planet_eccentric_anomaly, planet_eccentricity)
     planet_longitude = (planet_true_anomaly + planet_longitude_of_perihelion) % 360
     planet_ecliptic_coords = ecliptic_coords(planet_distance_from_primary, planet_longitude_of_the_ascending_node, planet_true_anomaly, planet_argument_of_perihelion, planet_inclination)
-    puts "X Ecliptic Coord = #{planet_ecliptic_coords[0]}"
-    puts "Y Ecliptic Coord = #{planet_ecliptic_coords[1]}"
-    puts "Z Ecliptic Coord = #{planet_ecliptic_coords[2]}"
     planet_argument_of_latitude = argument_of_latitude(planet_mean_longitude, planet_true_anomaly, planet_mean_anomaly, planet_longitude_of_the_ascending_node)
-    puts "u = #{planet_argument_of_latitude}"
     planet_ecliptic_latitude = ecliptic_latitude(planet_argument_of_latitude, planet_inclination)
     planet_heliocentric_coords = heliocentric_coorinates(planet_distance_from_primary, planet_longitude, planet_ecliptic_latitude)
-    puts ""
-    puts "Coordinates #{self.name}:"
-    puts "d = #{planet_distance_from_primary}"
-    puts "Latitude (b) = #{planet_ecliptic_latitude}"
-    puts "Longitude (lon) = #{planet_longitude}"
-    puts "#{planet_heliocentric_coords}"
-    puts ""
     output_coords = planet_heliocentric_coords
     output_coords << planet_longitude
     output_coords << planet_ecliptic_latitude
-    puts "#{output_coords}"
-    # output is [x, y, z, longitude, latitude]
+    output_coords << planet_distance_from_primary
+    # output is [x, y, z, longitude, latitude, distance]
     return output_coords
   end
 
