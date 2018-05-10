d8c52cd3287a0cec11c593363555ae0c877232ca
Tim Hollis <tim.hollis@gmail.com>
2018-05-10 12:08:16 -0400

    react renders in erb, set up FlightContainer

---
+++ app/javascript/containers/FlightContainer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class FlightContainer extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return(
+      <div>
+        <h4>Flight Form goes here</h4>
+        <h4>Flight Info goes here</h4>
+      </div>
+    )
+  }
+}
+
+export default FlightContainer;
