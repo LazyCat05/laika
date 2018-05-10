de29c904ba18d75eeb9336a4bbec3212fb8ef246
Tim Hollis <tim.hollis@gmail.com>
2018-05-10 14:09:44 -0400

    built new flight form

--- app/javascript/containers/FlightContainer.js
+++ app/javascript/containers/FlightContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import FlightForm from './FlightForm'
 
 class FlightContainer extends React.Component {
   constructor(props){
@@ -7,10 +8,33 @@ class FlightContainer extends React.Component {
 
     }
   }
+
+  addNewFlight(formPayload) {
+    fetch('/api/v1/flights.json', {
+      credentials: 'same-origin',
+      method: 'post',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(formPayload)
+    })
+      .then(response => {
+        if(response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`, error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .then(response => response.json())
+      .then(responseFlight => {
+        this.setState({ flight: responseFlight})
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }
   render() {
     return(
       <div>
-        <h4>Flight Form goes here</h4>
+        <FlightForm
+        addNewFlight = {this.addNewFlight}/>
         <h4>Flight Info goes here</h4>
       </div>
     )
