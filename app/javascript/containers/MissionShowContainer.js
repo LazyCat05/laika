dd2e6339de1d04b66ed2279a05254d230757a4d2
Tim Hollis <tim.hollis@gmail.com>
2018-05-15 15:19:14 -0400

    added flight listing and flight form to mission show page

--- app/javascript/containers/MissionShowContainer.js
+++ app/javascript/containers/MissionShowContainer.js
@@ -35,8 +35,31 @@ class MissionShowContainer extends React.Component {
   render(){
     console.log(this.state.mission)
     console.log(this.state.flights)
+    let flights = this.state.flights.map(flight => {
+      return(
+      <FlightInfo
+        key = {flight.delta_v}
+        departure_date = {flight.departure_date}
+        arrival_date = {flight.arrival_date}
+        origin = {flight.origin_planet}
+        originCoordinates = {flight.origin_coordinates}
+        destination = {flight.destination_planet}
+        destinationCoordinates = {flight.destination_coordinates}
+        distance = {flight.distance}
+        deltaV = {flight.delta_v}
+        angularSeparation = {flight.angular_separation}
+        launchDate = {flight.launch_date}
+        flight_time = {flight.time_of_flight}
+      />
+    )
+    })
+
     return(
-      <div></div>
+      <div className='row'>
+        <div className= 'columns small-10 medium-5'>
+          {flights}
+        </div>
+      </div>
     )
   }
 
